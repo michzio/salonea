1228cf1ce010bc6af543098ebbe72eb96a33679d
michzio <michzio@hotmail.com>
2016-01-09 22:36:35 +0100

    jax-rs SkillResource.createSkill

--- src/main/java/pl/salonea/jaxrs/SkillResource.java
+++ src/main/java/pl/salonea/jaxrs/SkillResource.java
@@ -7,11 +7,17 @@ import pl.salonea.entities.Skill;
 import pl.salonea.jaxrs.bean_params.EmployeeBeanParam;
 import pl.salonea.jaxrs.bean_params.GenericBeanParam;
 import pl.salonea.jaxrs.bean_params.SkillBeanParam;
+import pl.salonea.jaxrs.exceptions.*;
+import pl.salonea.jaxrs.exceptions.BadRequestException;
+import pl.salonea.jaxrs.exceptions.ForbiddenException;
+import pl.salonea.jaxrs.exceptions.NotFoundException;
 import pl.salonea.jaxrs.utils.RESTToolkit;
 import pl.salonea.jaxrs.utils.ResourceList;
 import pl.salonea.jaxrs.utils.hateoas.Link;
 import pl.salonea.jaxrs.wrappers.SkillWrapper;
 
+import javax.ejb.EJBException;
+import javax.ejb.EJBTransactionRolledbackException;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -19,11 +25,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import pl.salonea.jaxrs.exceptions.ForbiddenException;
-import pl.salonea.jaxrs.exceptions.NotFoundException;
-import pl.salonea.jaxrs.exceptions.BadRequestException;
 
 /**
  * Created by michzio on 07/01/2016.
@@ -134,7 +138,89 @@ public class SkillResource {
         return Response.status(Status.OK).entity(skills).build();
     }
 
+    /**
+     * Method matches specific Skill resource by identifier and returns its instance
+     */
+    @GET
+    @Path("/{skillId : \\d+}") // catch only numeric identifiers
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response getSkill( @PathParam("skillId") Integer skillId,
+                              @BeanParam GenericBeanParam params ) throws ForbiddenException, NotFoundException {
 
+        RESTToolkit.authorizeAccessToWebService(params);
+        logger.log(Level.INFO, "returning given Skill by executing SkillResource.getSkill(skillId) method of REST API");
+
+        Skill foundSkill = skillFacade.find(skillId);
+        if(foundSkill == null)
+            throw new NotFoundException("Could not find skill for id " + skillId + ".");
+
+        // adding hypermedia links to skill resource
+        SkillResource.populateWithHATEOASLinks(foundSkill, params.getUriInfo());
+
+        return Response.status(Status.OK).entity(foundSkill).build();
+    }
+
+    /**
+     * Method matches specific Skill resource by identifier and returns its instance fetching it eagerly
+     */
+    @GET
+    @Path("/{skillId : \\d+}/eagerly")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response getSkillEagerly( @PathParam("skillId") Integer skillId,
+                                     @BeanParam GenericBeanParam params ) throws ForbiddenException, NotFoundException {
+
+        RESTToolkit.authorizeAccessToWebService(params);
+        logger.log(Level.INFO, "returning given Skill eagerly by executing SkillResource.getSkillEagerly(skillId) method of REST API");
+
+        Skill foundSkill = skillFacade.findByIdEagerly(skillId);
+        if(foundSkill == null)
+            throw new NotFoundException("Could not find skill for id " + skillId + ".");
+
+        // wrapping Skill into SkillWrapper in order to marshal eagerly fetched associated collection of entities
+        SkillWrapper wrappedSkill = new SkillWrapper(foundSkill);
+
+        // adding hypermedia links to wrapped skill resource
+        SkillResource.populateWithHATEOASLinks(wrappedSkill, params.getUriInfo());
+
+        return Response.status(Status.OK).entity(wrappedSkill).build();
+    }
+
+    /**
+     * Method that takes Skill as XML or JSON and creates its new instance in database
+     */
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response createSkill( Skill skill,
+                                 @BeanParam GenericBeanParam params ) throws ForbiddenException, UnprocessableEntityException, InternalServerErrorException {
+
+        RESTToolkit.authorizeAccessToWebService(params);
+        logger.log(Level.INFO, "creating new Skill by executing SkillResource.createSkill(skill) method of REST API");
+
+        Skill createdSkill = null;
+        URI locationURI = null;
+
+        try {
+            // persist new resource in database
+            createdSkill = skillFacade.create(skill);
+
+            // populate created resource with hypermedia links
+            SkillResource.populateWithHATEOASLinks(createdSkill, params.getUriInfo());
+
+            // construct link to newly created resource to return in HTTP Header
+            String createdSkillId = String.valueOf(createdSkill.getSkillId());
+            locationURI = params.getUriInfo().getBaseUriBuilder().path(SkillResource.class).path(createdSkillId).build();
+
+        } catch (EJBTransactionRolledbackException ex) {
+            ExceptionHandler.handleEJBTransactionRolledbackException(ex);
+        } catch (EJBException ex) {
+            ExceptionHandler.handleEJBException(ex);
+        } catch (Exception ex) {
+            throw new InternalServerErrorException(ExceptionHandler.ENTITY_CREATION_ERROR_MESSAGE);
+        }
+
+        return Response.created(locationURI).entity(createdSkill).build();
+    }
 
     /**
      * related subresources (through relationships)
