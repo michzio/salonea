6e1af5eed5d0e2e2b3a029540aa3cfa90e903c8a
michzio <michzio@hotmail.com>
2016-01-25 20:04:37 +0100

    jax-rs WorkStationResource.EmployeeResource first draft

--- src/main/java/pl/salonea/jaxrs/WorkStationResource.java
+++ src/main/java/pl/salonea/jaxrs/WorkStationResource.java
@@ -2,24 +2,34 @@ package pl.salonea.jaxrs;
 
 import pl.salonea.ejb.stateless.EmployeeFacade;
 import pl.salonea.ejb.stateless.WorkStationFacade;
+import pl.salonea.entities.Employee;
+import pl.salonea.entities.ProviderService;
+
 import pl.salonea.entities.WorkStation;
+import pl.salonea.entities.idclass.WorkStationId;
+import pl.salonea.jaxrs.bean_params.DateBetweenBeanParam;
+import pl.salonea.jaxrs.bean_params.EmployeeBeanParam;
 import pl.salonea.jaxrs.bean_params.GenericBeanParam;
 import pl.salonea.jaxrs.bean_params.WorkStationBeanParam;
+import pl.salonea.jaxrs.exceptions.NotFoundException;
 import pl.salonea.jaxrs.exceptions.ForbiddenException;
+import pl.salonea.jaxrs.utils.RESTToolkit;
 import pl.salonea.jaxrs.utils.ResourceList;
 import pl.salonea.jaxrs.utils.hateoas.Link;
 import pl.salonea.jaxrs.wrappers.WorkStationWrapper;
 
 import javax.inject.Inject;
-import javax.transaction.UserTransaction;
-import javax.ws.rs.BeanParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.*;
+import javax.transaction.NotSupportedException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -124,8 +134,12 @@ public class WorkStationResource {
 
         WorkStationResource.populateWithHATEOASLinks(workStationWrapper.getWorkStation(), uriInfo);
 
-        // TODO
+      // TODO
+      //  for(TermEmployeeWorkOn employeeTerm : workStationWrapper.getTermsEmployeesWorkOn())
+      //      pl.salonea.jaxrs.EmployeeTermResource.populateWithHATEOASLinks(employeeTerm, uriInfo);
 
+        for(ProviderService providerService : workStationWrapper.getProvidedServices())
+            pl.salonea.jaxrs.ProviderServiceResource.populateWithHATEOASLinks(providerService, uriInfo);
     }
 
     /**
@@ -133,17 +147,157 @@ public class WorkStationResource {
      */
     public static void populateWithHATEOASLinks(WorkStation workStation, UriInfo uriInfo) {
 
-        // self link with pattern: http://localhost:port/app/rest/{resources}/{id}/{subresources}/{sub-id}/{sub-subresources}/{sub-sub-id}
+        try {
 
-        // self eagerly link with pattern: http://localhost:port/app/rest/{resources}/{id}/{subresources}/{sub-id}/{sub-subresources}/{sub-sub-id}/eagerly
+            // self link with pattern: http://localhost:port/app/rest/{resources}/{id}/{subresources}/{sub-id}/{sub-subresources}/{sub-sub-id}
 
-        // collection link with pattern: http://localhost:port/app/rest/{resources}
-        workStation.getLinks().add( Link.fromUri(uriInfo.getBaseUriBuilder()
-                                                        .path(WorkStationResource.class)
-                                                        .build())
-                                        .rel("work-stations").build() );
+            // collection link with pattern: http://localhost:port/app/rest/{resources}
+            workStation.getLinks().add(Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(WorkStationResource.class)
+                    .build())
+                    .rel("work-stations").build());
+
+            // self eagerly link with pattern: http://localhost:port/app/rest/{resources}/{id}/{subresources}/{sub-id}/{sub-subresources}/{sub-sub-id}/eagerly
+
+            // associated collections links with pattern: http://localhost:port/app/rest/{resources}/{id}/{relationship}
+
+            /**
+             * Provider Services associated with current Work Station resource
+             */
+
+            /**
+             * Employee Terms (TermEmployeeWorkOn entity) associated with current Work Station resource
+             */
+
+            /**
+             * Employees working on current Work Station resource
+             */
+
+            // employees
+            Method employeesMethod = WorkStationResource.class.getMethod("getEmployeeResource");
+            workStation.getLinks().add(Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(WorkStationResource.class)
+                    .path(employeesMethod)
+                    .resolveTemplate("providerId", workStation.getServicePoint().getProvider().getUserId().toString())
+                    .resolveTemplate("servicePointNumber", workStation.getServicePoint().getServicePointNumber().toString())
+                    .resolveTemplate("workStationNumber", workStation.getWorkStationNumber().toString())
+                    .build())
+                    .rel("employees").build());
+
+            // employees eagerly
+            Method employeesEagerlyMethod = WorkStationResource.EmployeeResource.class.getMethod("getWorkStationEmployeesEagerly", Long.class, Integer.class, Integer.class, EmployeeBeanParam.class);
+            workStation.getLinks().add(Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(WorkStationResource.class)
+                    .path(employeesMethod)
+                    .path(employeesEagerlyMethod)
+                    .resolveTemplate("providerId", workStation.getServicePoint().getProvider().getUserId().toString())
+                    .resolveTemplate("servicePointNumber", workStation.getServicePoint().getServicePointNumber().toString())
+                    .resolveTemplate("workStationNumber", workStation.getWorkStationNumber().toString())
+                    .build())
+                    .rel("employees-eagerly").build());
+
+            // employees count
+            Method countEmployeesByWorkStationMethod = WorkStationResource.EmployeeResource.class.getMethod("countEmployeesByWorkStation", Long.class, Integer.class, Integer.class, GenericBeanParam.class);
+            workStation.getLinks().add(Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(WorkStationResource.class)
+                    .path(employeesMethod)
+                    .path(countEmployeesByWorkStationMethod)
+                    .resolveTemplate("providerId", workStation.getServicePoint().getProvider().getUserId().toString())
+                    .resolveTemplate("servicePointNumber", workStation.getServicePoint().getServicePointNumber().toString())
+                    .resolveTemplate("workStationNumber", workStation.getWorkStationNumber().toString())
+                    .build())
+                    .rel("employees-count").build());
+
+            // employees by-term
+            Method employeesByTermMethod = WorkStationResource.EmployeeResource.class.getMethod("getWorkStationEmployeesByTerm", Long.class, Integer.class, Integer.class, DateBetweenBeanParam.class);
+            workStation.getLinks().add(Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(WorkStationResource.class)
+                    .path(employeesMethod)
+                    .path(employeesByTermMethod)
+                    .resolveTemplate("providerId", workStation.getServicePoint().getProvider().getUserId().toString())
+                    .resolveTemplate("servicePointNumber", workStation.getServicePoint().getServicePointNumber().toString())
+                    .resolveTemplate("workStationNumber", workStation.getWorkStationNumber().toString())
+                    .build())
+                    .rel("employees-by-term").build());
+
+            // employees by-term-strict
+            Method employeesByTermStrictMethod = WorkStationResource.EmployeeResource.class.getMethod("getWorkStationEmployeesByTermStrict", Long.class, Integer.class, Integer.class, DateBetweenBeanParam.class);;
+            workStation.getLinks().add(Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(WorkStationResource.class)
+                    .path(employeesMethod)
+                    .path(employeesByTermStrictMethod)
+                    .resolveTemplate("providerId", workStation.getServicePoint().getProvider().getUserId().toString())
+                    .resolveTemplate("servicePointNumber", workStation.getServicePoint().getServicePointNumber().toString())
+                    .resolveTemplate("workStationNumber", workStation.getWorkStationNumber().toString())
+                    .build())
+                    .rel("employees-by-term-strict").build());
+
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
         // TODO
+    }
+
+    public class EmployeeResource {
+
+        public EmployeeResource() { }
+
+        /**
+         * Method returns subset of Employee entities for given Work Station entity.
+         * The provider id, service point number and work station number are passed through path params.
+         * They can be additionally filtered and paginated by @QueryParams.
+         */
+        @GET
+        @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+        public Response getWorkStationEmployees( @PathParam("providerId") Long providerId,
+                                                 @PathParam("servicePointNumber") Integer servicePointNumber,
+                                                 @PathParam("workStationNumber") Integer workStationNumber,
+                                                 @BeanParam EmployeeBeanParam params ) throws ForbiddenException, NotFoundException,
+        /* UserTransaction exceptions */ HeuristicRollbackException, RollbackException, HeuristicMixedException, SystemException, NotSupportedException {
+
+            RESTToolkit.authorizeAccessToWebService(params);
+            logger.log(Level.INFO, "returning employees for given work station using " +
+                    "WorkStationResource.EmployeeResource.getWorkStationEmployees(providerId, servicePointNumber, workStationNumber) method of REST API");
+
+            utx.begin();
+
+            // find work station entity for which to get associated employees
+            WorkStation workStation = workStationFacade.find( new WorkStationId(providerId, servicePointNumber, workStationNumber) );
+            if(workStation == null)
+                throw new NotFoundException("Could not find work station for id (" + providerId + "," + servicePointNumber + "," + workStationNumber + ").");
+
+            Integer noOfParams = RESTToolkit.calculateNumberOfFilterQueryParams(params);
+
+            ResourceList<Employee> employees = null;
+
+            if(noOfParams > 0) {
+                logger.log(Level.INFO, "There is at least one filter query param in HTTP request.");
+
+                List<WorkStation> workStations = new ArrayList<>();
+                workStations.add(workStation);
+
+                // get employees for given work station filtered by given params
+                employees = new ResourceList<>(
+                        employeeFacade.findByMultipleCriteria(params.getDescriptions(), params.getJobPositions(), params.getSkills(),
+                                params.getEducations(), params.getServices(), params.getProviderServices(), params.getServicePoints(),
+                                workStations, params.getPeriod(), params.getStrictTerm(), params.getRated(), params.getMinAvgRating(),
+                                params.getMaxAvgRating(), params.getRatingClients(), params.getOffset(), params.getLimit())
+                );
+            } else {
+                logger.log(Level.INFO, "There isn't any filter query param in HTTP request.");
+
+                // get employees for given work station without filtering (eventually paginated)
+                employees = new ResourceList<>( employeeFacade.findByWorkStation(workStation, params.getOffset(), params.getLimit()) );
+            }
+
+            utx.commit();
+
+            // result resources need to be populated with hypermedia links to enable resource discovery
+            pl.salonea.jaxrs.EmployeeResource.populateWithHATEOASLinks(employees, params.getUriInfo(), params.getOffset(), params.getLimit());
+
+            return Response.status(Status.OK).entity(employees).build();
+        }
     }
 
 }
