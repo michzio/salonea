753ff99d6729230c0f9559c922086ce82721de0b
michzio <michzio@hotmail.com>
2016-01-25 23:31:42 +0100

    jax-rs WorkStationResource.EmployeeResource completed

--- src/main/java/pl/salonea/jaxrs/WorkStationResource.java
+++ src/main/java/pl/salonea/jaxrs/WorkStationResource.java
@@ -11,11 +11,14 @@ import pl.salonea.jaxrs.bean_params.DateBetweenBeanParam;
 import pl.salonea.jaxrs.bean_params.EmployeeBeanParam;
 import pl.salonea.jaxrs.bean_params.GenericBeanParam;
 import pl.salonea.jaxrs.bean_params.WorkStationBeanParam;
-import pl.salonea.jaxrs.exceptions.NotFoundException;
+import pl.salonea.jaxrs.exceptions.BadRequestException;
 import pl.salonea.jaxrs.exceptions.ForbiddenException;
+import pl.salonea.jaxrs.exceptions.NotFoundException;
 import pl.salonea.jaxrs.utils.RESTToolkit;
 import pl.salonea.jaxrs.utils.ResourceList;
+import pl.salonea.jaxrs.utils.ResponseWrapper;
 import pl.salonea.jaxrs.utils.hateoas.Link;
+import pl.salonea.jaxrs.wrappers.EmployeeWrapper;
 import pl.salonea.jaxrs.wrappers.WorkStationWrapper;
 
 import javax.inject.Inject;
@@ -290,6 +293,179 @@ public class WorkStationResource {
                 // get employees for given work station without filtering (eventually paginated)
                 employees = new ResourceList<>( employeeFacade.findByWorkStation(workStation, params.getOffset(), params.getLimit()) );
             }
+
+            utx.commit();
+
+            // result resources need to be populated with hypermedia links to enable resource discovery
+            pl.salonea.jaxrs.EmployeeResource.populateWithHATEOASLinks(employees, params.getUriInfo(), params.getOffset(), params.getLimit());
+
+            return Response.status(Status.OK).entity(employees).build();
+        }
+
+        /**
+         * Method returns subset of Employee entities for given Work Station fetching them eagerly.
+         * The provider id, service point number and work station number are passed through path params.
+         * They can be additionally filtered and paginated by @QueryParams.
+         */
+        @GET
+        @Path("/eagerly")
+        @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+        public Response getWorkStationEmployeesEagerly( @PathParam("providerId") Long providerId,
+                                                        @PathParam("servicePointNumber") Integer servicePointNumber,
+                                                        @PathParam("workStationNumber") Integer workStationNumber,
+                                                        @BeanParam EmployeeBeanParam params ) throws ForbiddenException, NotFoundException,
+        /* UserTransaction exceptions */ HeuristicRollbackException, RollbackException, HeuristicMixedException, SystemException, NotSupportedException {
+
+            RESTToolkit.authorizeAccessToWebService(params);
+            logger.log(Level.INFO, "returning employees eagerly for given work station using " +
+                    "WorkStationResource.EmployeeResource.getWorkStationEmployeesEagerly(providerId, servicePointNumber, workStationNumber) method of REST API");
+
+            utx.begin();
+
+            // find work station entity for which to get associated employees
+            WorkStation workStation = workStationFacade.find( new WorkStationId(providerId, servicePointNumber, workStationNumber) );
+            if(workStation == null)
+                throw new NotFoundException("Could not find work station for id (" + providerId + "," + servicePointNumber + "," + workStationNumber + ").");
+
+            Integer noOfParams = RESTToolkit.calculateNumberOfFilterQueryParams(params);
+
+            ResourceList<EmployeeWrapper> employees = null;
+
+            if(noOfParams > 0) {
+                logger.log(Level.INFO, "There is at least one filter query param in HTTP request.");
+
+                List<WorkStation> workStations = new ArrayList<>();
+                workStations.add(workStation);
+
+                // get employees eagerly for given work station filtered by given params
+                employees = new ResourceList<>(
+                        EmployeeWrapper.wrap(
+                                employeeFacade.findByMultipleCriteriaEagerly(params.getDescriptions(), params.getJobPositions(), params.getSkills(),
+                                        params.getEducations(), params.getServices(), params.getProviderServices(), params.getServicePoints(),
+                                        workStations, params.getPeriod(), params.getStrictTerm(), params.getRated(), params.getMinAvgRating(),
+                                        params.getMaxAvgRating(), params.getRatingClients(), params.getOffset(), params.getLimit())
+                        )
+                );
+            } else {
+                logger.log(Level.INFO, "There isn't any filter query param in HTTP request.");
+
+                // get employees eagerly for given work station without filtering (eventually paginated)
+                employees = new ResourceList<>( EmployeeWrapper.wrap(employeeFacade.findByWorkStationEagerly(workStation, params.getOffset(), params.getLimit())) );
+            }
+
+            utx.commit();
+
+            // result resources need to be populated with hypermedia links to enable resource discovery
+            pl.salonea.jaxrs.EmployeeResource.populateWithHATEOASLinks(employees, params.getUriInfo(), params.getOffset(), params.getLimit());
+
+            return Response.status(Status.OK).entity(employees).build();
+        }
+
+        /**
+         * Method counts Employee entities for given Work Station entity.
+         * The provider id, service point number and work station number are passed through path params.
+         */
+        @GET
+        @Path("/count")
+        @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+        public Response countEmployeesByWorkStation( @PathParam("providerId") Long providerId,
+                                                     @PathParam("servicePointNumber") Integer servicePointNumber,
+                                                     @PathParam("workStationNumber") Integer workStationNumber,
+                                                     @BeanParam GenericBeanParam params ) throws ForbiddenException, NotFoundException,
+        /* UserTransaction exceptions */ HeuristicRollbackException, RollbackException, HeuristicMixedException, SystemException, NotSupportedException {
+
+            RESTToolkit.authorizeAccessToWebService(params);
+            logger.log(Level.INFO, "returning number of employees for given work station by executing " +
+                    "WorkStationResource.EmployeeResource.countEmployeesByWorkStation(providerId, servicePointNumber, workStationNumber) method of REST API");
+
+            utx.begin();
+
+            // find work station entity for which to count employees
+            WorkStation workStation = workStationFacade.find( new WorkStationId(providerId, servicePointNumber, workStationNumber) );
+            if(workStation == null)
+                throw new NotFoundException("Could not find work station for id (" + providerId + "," + servicePointNumber + "," + workStationNumber + ").");
+
+            ResponseWrapper responseEntity = new ResponseWrapper(String.valueOf(employeeFacade.countByWorkStation(workStation)), 200,
+                    "number of employees for work station with id (" + providerId + "," + servicePointNumber + "," + workStationNumber + ").");
+
+            utx.commit();
+
+            return Response.status(Status.OK).entity(responseEntity).build();
+        }
+
+        /**
+         * Method returns subset of Employee entities for given Work Station entity and
+         * Term they work on it. The provider id, service point number and work station number
+         * are passed through path params. Term start and end dates are passed through query params.
+         */
+        @GET
+        @Path("/by-term")
+        @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+        public Response getWorkStationEmployeesByTerm( @PathParam("providerId") Long providerId,
+                                                       @PathParam("servicePointNumber") Integer servicePointNumber,
+                                                       @PathParam("workStationNumber") Integer workStationNumber,
+                                                       @BeanParam DateBetweenBeanParam params ) throws ForbiddenException, NotFoundException, BadRequestException,
+        /* UserTransaction exceptions */ HeuristicRollbackException, RollbackException, HeuristicMixedException, SystemException, NotSupportedException {
+
+            RESTToolkit.authorizeAccessToWebService(params);
+            logger.log(Level.INFO, "returning employees for given work station and term (startDate, endDate) using " +
+                    "WorkStationResource.EmployeeResource.getWorkStationEmployeesByTerm(providerId, servicePointNumber, workStationNumber, term) method of REST API");
+
+            RESTToolkit.validateDateRange(params); // i.e. startDate and endDate
+
+            utx.begin();
+
+            // find work station entity for which to get associated employees
+            WorkStation workStation = workStationFacade.find( new WorkStationId(providerId, servicePointNumber, workStationNumber) );
+            if(workStation == null)
+                throw new NotFoundException("Could not find work station for id (" + providerId + "," + servicePointNumber + "," + workStationNumber + ").");
+
+            // find employees by given criteria (work station, term)
+            ResourceList<Employee> employees =  new ResourceList<>(
+                    employeeFacade.findByWorkStationAndTerm(workStation, params.getStartDate(), params.getEndDate(),
+                            params.getOffset(), params.getLimit())
+            );
+
+            utx.commit();
+
+            // result resources need to be populated with hypermedia links to enable resource discovery
+            pl.salonea.jaxrs.EmployeeResource.populateWithHATEOASLinks(employees, params.getUriInfo(), params.getOffset(), params.getLimit());
+
+            return Response.status(Status.OK).entity(employees).build();
+        }
+
+        /**
+         * Method returns subset of Employee entities for given Work Station entity and
+         * Term (strict) they work on it. The provider id, service point number and work station number
+         * are passed through path params. Term (strict) start and end dates are passed through query params.
+         */
+        @GET
+        @Path("/by-term-strict")
+        @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+        public Response getWorkStationEmployeesByTermStrict( @PathParam("providerId") Long providerId,
+                                                             @PathParam("servicePointNumber") Integer servicePointNumber,
+                                                             @PathParam("workStationNumber") Integer workStationNumber,
+                                                             @BeanParam DateBetweenBeanParam params ) throws ForbiddenException, NotFoundException, BadRequestException,
+        /* UserTransaction exceptions */ HeuristicRollbackException, RollbackException, HeuristicMixedException, SystemException, NotSupportedException {
+
+            RESTToolkit.authorizeAccessToWebService(params);
+            logger.log(Level.INFO, "returning employees for given work station and term strict (startDate, endDate) using " +
+                    "WorkStationResource.EmployeeResource.getWorkStationEmployeesByTermStrict(providerId, servicePointNumber, workStationNumber, termStrict) method of REST API");
+
+            RESTToolkit.validateDateRange(params); // i.e. startDate and endDate
+
+            utx.begin();
+
+            // find work station entity for which to get associated employees
+            WorkStation workStation = workStationFacade.find( new WorkStationId(providerId, servicePointNumber, workStationNumber) );
+            if(workStation == null)
+                throw new NotFoundException("Could not find work station for id (" + providerId + "," + servicePointNumber + "," + workStationNumber + ").");
+
+            // find employees by given criteria (work station, term strict)
+            ResourceList<Employee> employees =  new ResourceList<>(
+                    employeeFacade.findByWorkStationAndTermStrict(workStation, params.getStartDate(), params.getEndDate(),
+                            params.getOffset(), params.getLimit())
+            );
 
             utx.commit();
 
